9d731551fd6f03e36a857d00a8565b86a1e0a7a0
RamRaid <71435969+ramraid319@users.noreply.github.com>
2024-05-17 19:15:49 +0900

    init

---
+++ Source/PROD_Wakgood/PROD_Wakgood.cpp
@@ -0,0 +1,6 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PROD_Wakgood.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PROD_Wakgood, "PROD_Wakgood" );
