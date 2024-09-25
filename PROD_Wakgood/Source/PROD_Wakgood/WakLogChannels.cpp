523b9116db56ae1f9bc9482c5c9144fb81e038aa
UpDownGo <dkfhddla@naver.com>
2024-09-25 23:42:58 +0900

    feat(character): add log channels.h

---
+++ PROD_Wakgood/Source/PROD_Wakgood/WakLogChannels.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WakLogChannels.h"
+#include "GameFramework/Actor.h"
+
+DEFINE_LOG_CATEGORY(LogWak);
+DEFINE_LOG_CATEGORY(LogWakAbilitySystem);
---
+++ PROD_Wakgood/Source/PROD_Wakgood/WakLogChannels.h
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Logging/LogMacros.h"
+
+class UObject;
+
+PROD_WAKGOOD_API DECLARE_LOG_CATEGORY_EXTERN(LogWak, Log, All);
+PROD_WAKGOOD_API DECLARE_LOG_CATEGORY_EXTERN(LogWakAbilitySystem, Log, All);
