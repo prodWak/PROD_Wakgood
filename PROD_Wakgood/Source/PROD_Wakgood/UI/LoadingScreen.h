fff4c5e3c70099801a9318eba00ff0cc0997b4fb
Edge-NewGame <studio.superplus@gmail.com>
2025-02-18 23:53:28 +0900

    feat : 메인 타이틀 ui 작업

---
+++ PROD_Wakgood/Source/PROD_Wakgood/UI/LoadingScreen.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Engine.h
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+// Generated
+#include "LoadingScreen.generated.h"
+
+UCLASS()
+class PROD_WAKGOOD_API ALoadingScreen : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+	ALoadingScreen();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	void RemoveLoadingScreen();
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "MenuUI", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUserWidget> LoadingScreenClass;
+	
+	UPROPERTY()
+	UUserWidget* LoadingScreenWidget;
+};
---
+++ PROD_Wakgood/Source/PROD_Wakgood/UI/MainMenuUI.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Engine.h
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+// Generated
+#include "MainMenuUI.generated.h"
+
+// Forward Declaration
+class UButton;
+
+UCLASS()
+class PROD_WAKGOOD_API AMainMenuUI : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+	AMainMenuUI();
+
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	void InitInput();
+	void ButtonFocus(int32 buttonIndex);
+	void ProcessW();
+	void ProcessS();
+	// void ProcessA();
+	// void ProcessD();
+	void ProcessJ();
+
+	UFUNCTION()
+	void OnStoryButtonClicked();
+	
+	UFUNCTION()
+	void SetHoveredButton(UButton* hoveredButton, bool bIsHovered);
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "MenuUI")
+	TSubclassOf<UUserWidget> MainMenuClass;
+	
+	UPROPERTY()
+	TObjectPtr<UUserWidget> MainMenuWidget;
+	
+	UPROPERTY()
+	TObjectPtr<APlayerController> MainMenuController;
+
+	UPROPERTY()
+	TArray<TObjectPtr<UButton>> MainMenuButtons;
+	
+	int32 CurrentButtonIndex = 0;
+	int32 TargetButtonIndex = 0;
+};
