13a0861b0db0f6bf0d53d520eeac3f54706f74a9
Edge-NewGame <studio.superplus@gmail.com>
2025-02-18 23:53:28 +0900

    Test

---
+++ PROD_Wakgood/Source/PROD_Wakgood/UI/GameModeMenuUI.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Engine.h
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+// Generated
+#include "GameModeMenuUI.generated.h"
+
+UCLASS()
+class PROD_WAKGOOD_API AGameModeMenuUI : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameModeMenuUI();
+
+public:
+	void SwitchToLoadingScreen();
+	void SwitchToMainMenuUI();
+};
